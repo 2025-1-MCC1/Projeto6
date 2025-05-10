63796857c1cde07e38470fb77dc3d6180014bb13
Rafaela Morais <155861986+rafafmorais@users.noreply.github.com>
2025-05-10 18:43:38 -0300

    Radio
    Primeira parte da fala do radio

---
+++ src/There is a Monster in the Window/Assets/Scripts/Radio.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class Radio : MonoBehaviour
+{
+    public GameObject RadioUI;
+    public TextMeshProUGUI textoDialogo;      
+    public string[] falasRadio;           
+    public string[] falasJogador;              
+    public float tempoEntreFalas = 1f;// Tempo entre cada fala completa
+    public float typeDelay = 0.005f;// Tempo entre cada letra
+    public Color corRadio = Color.green;
+    public Color corPlayer = Color.white;     
+
+    private bool dialogoIniciado = false;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !dialogoIniciado)
+        {
+            dialogoIniciado = true;
+            RadioUI.SetActive(true);
+            StartCoroutine(ExecutarDialogo());
+        }
+    }
+
+    private IEnumerator ExecutarDialogo()
+    {
+        RadioUI.SetActive(true);
+
+        // Falas do rádio
+        foreach (string fala in falasRadio)
+        {
+            textoDialogo.color = corRadio;
+            yield return StartCoroutine(DigitarTexto(fala));
+            yield return new WaitForSeconds(tempoEntreFalas);
+        }
+        // Falas do jogador
+        foreach (string fala in falasJogador)
+        {
+            textoDialogo.color = corPlayer;
+            yield return StartCoroutine(DigitarTexto(fala));
+            yield return new WaitForSeconds(tempoEntreFalas);
+        }
+
+        RadioUI.SetActive(false);
+    }
+    private IEnumerator DigitarTexto(string frase) 
+    {
+        textoDialogo.text = "";
+        foreach (char letra in frase.ToCharArray())
+        {
+            textoDialogo.text += letra;
+            yield return new WaitForSeconds(typeDelay);
+        }
+    }
+    void Start()
+    {        
+
+    }
+
+    void Update()
+    {
+
+    }
+
+}
